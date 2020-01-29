d1aa82a29fc35d38fa389793d3cbf6396c48a619
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-29 03:22:24 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,18 @@
 def starts_with_a_vowel?(word)
-
+  word.match \/A[aeiou]/i
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan  /\w/un+ing[UN+ING]/i
 end
 
 def words_five_letters_long(text)
+  text.scan /\d\+[words_five_letters_long]/i  
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match\/w+d\[/w+/d]
 
 end
 
